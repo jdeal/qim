68f3852f6d491aaf87ea74df3396907b3e011b1c
Justin Deal <justin.deal@zapier.com>
2017-03-19 14:42:26 -0500

    setIn benchmark

---
+++ benchmarks/setIn.js
@@ -0,0 +1,57 @@
+import fp from 'lodash/fp';
+import update from 'immutability-helper';
+
+import {setIn} from '../src';
+
+const state = {
+  users: {
+    mary: {
+      name: {
+        first: 'Mary',
+        last: 'Bar'
+      },
+      friends: [],
+      balance: 1000
+    },
+    joe: {
+      name: {
+        first: 'Joe',
+        last: 'Foo'
+      },
+      friends: [],
+      balance: 100
+    }
+  }
+};
+
+export default [
+  {
+    name: 'lodash fp set',
+    test: () => (
+      fp.set(['users', 'joe', 'name', 'first'], 'Joseph', state)
+    ),
+    key: 'lodashFpSet'
+  },
+  {
+    name: 'immutability-helper',
+    test: () => (
+      update(state, {
+        users: {
+          joe: {
+            name: {
+              first: {$set: 'Joseph'}
+            }
+          }
+        }
+      })
+    ),
+    key: 'immutabilityHelper'
+  },
+  {
+    name: 'qim setIn',
+    test: () => setIn(['users', 'joe', 'name', 'first'], 'Joseph', state),
+    compare: {
+      lodashFpSet: 3
+    }
+  }
+];
