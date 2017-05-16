01b704d5e6e12e78943d734372733c5ab7a557bd
Justin Deal <justin.deal@gmail.com>
2017-05-16 12:32:23 -0500

    trying rollup with external object-assign

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,15 @@
-import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
+// import resolve from 'rollup-plugin-node-resolve';
+// import commonjs from 'rollup-plugin-commonjs';
 
 export default {
   entry: 'src/index.js',
   format: 'cjs',
   plugins: [
-    resolve(),
-    commonjs()
+    // resolve(),
+    // commonjs()
+  ],
+  external: [
+    'object-assign'
   ],
   dest: 'build/bundle.js'
 };
