a101a37c33dd045788e6f3017f2f232f41062e48
Justin Deal <justin.deal@gmail.com>
2017-05-16 12:35:38 -0500

    rollup to index.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,8 @@
-// import resolve from 'rollup-plugin-node-resolve';
-// import commonjs from 'rollup-plugin-commonjs';
-
 export default {
   entry: 'src/index.js',
   format: 'cjs',
-  plugins: [
-    // resolve(),
-    // commonjs()
-  ],
   external: [
     'object-assign'
   ],
-  dest: 'build/bundle.js'
+  dest: 'build/index.js'
 };
