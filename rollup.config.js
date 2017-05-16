a9cca7cffc6e395717165c2acf981f7e82b36490
Justin Deal <justin.deal@gmail.com>
2017-05-16 13:42:48 -0500

    undo busted rollup module, will fix in next release

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,28 @@
+import babel from 'rollup-plugin-babel';
+
 export default {
   entry: 'src/index.js',
   format: 'cjs',
   external: [
     'object-assign'
+  ],
+  plugins: [
+    babel({
+      babelrc: false,
+      exclude: 'node_modules/**',
+      "presets": [
+        [
+          "es2015",
+          {
+            modules: false
+          }
+        ],
+        "stage-2"
+      ],
+      "plugins": [
+        "external-helpers"
+      ]
+    }),
   ],
   dest: 'build/index.js'
 };
