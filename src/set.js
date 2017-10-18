dffebb6201fc7b57c077b2417228150aa8da5636
Justin Deal <justin.deal@gmail.com>
2017-10-17 23:30:39 -0500

    use spec in optimized set

--- src/set.js
+++ src/set.js
@@ -1,27 +1,16 @@
-import copy from './utils/copy';
 import {updateKey} from './$traverse';
 import {traverseEach} from './traverse';
 import {curry3} from './utils/curry';
 import {undefinedIfNone} from './$none';
-import {unwrap} from './utils/data';
+import {unwrap, getSpec} from './utils/data';
 
 const set = (path, value, obj) => {
 
   // Optimized case for a single primitive key.
   if (!path || typeof path !== 'object') {
-    if (obj == null || typeof obj !== 'object') {
-      return obj;
-    }
-
-    if (obj[path] === value) {
-      return obj;
-    }
-
-    obj = copy(obj);
-
-    obj[path] = value;
-
-    return obj;
+    const spec = getSpec(obj);
+    const _set = spec.set;
+    return _set(path, value, obj);
   }
 
   path = Array.isArray(path) ? path : [path];
--- src/utils/copy.js
+++
@@ -1,7 +0,0 @@
-import objectAssign from 'object-assign';
-
-const copy = (object) => Array.isArray(object) ?
-  object.slice(0) :
-  objectAssign({}, object);
-
-export default copy;
