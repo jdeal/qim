96f5a0d0e921bc07ede85c9f2bf3487b78de49dd
Justin Deal <justin.deal@gmail.com>
2017-11-01 22:07:32 -0500

    add in a bunch of select tests for $at

--- test/$at.js
+++ test/$at.js
@@ -4,63 +4,95 @@ import util from 'util';
 import 'babel-core/register';
 
 import {
+  select,
   update,
   $at,
   $set,
   $none,
 } from 'qim/src';
 
-const atMacro = (t, index, value, input, expected) => {
+const selectAtMacro = (t, index, input, expected) => {
+  const result = select([$at(index)], input);
+  t.deepEqual(result, expected);
+};
+
+test(selectAtMacro, 0, ['a', 'b', 'c'], ['a']);
+test(selectAtMacro, 1, ['a', 'b', 'c'], ['b']);
+test(selectAtMacro, -1, ['a', 'b', 'c'], ['c']);
+test(selectAtMacro, -2, ['a', 'b', 'c'], ['b']);
+test(selectAtMacro, 3, ['a', 'b', 'c'], [undefined]);
+test(selectAtMacro, -4, ['a', 'b', 'c'], [undefined]);
+
+test(selectAtMacro, 0, {a: 1, b: 2, c: 3}, [1]);
+test(selectAtMacro, 1, {a: 1, b: 2, c: 3}, [2]);
+test(selectAtMacro, -1, {a: 1, b: 2, c: 3}, [3]);
+test(selectAtMacro, -2, {a: 1, b: 2, c: 3}, [2]);
+test(selectAtMacro, 3, {a: 1, b: 2, c: 3}, [undefined]);
+test(selectAtMacro, -4, {a: 1, b: 2, c: 3}, [undefined]);
+
+test(selectAtMacro, 0, 'abc', ['a']);
+test(selectAtMacro, 1, 'abc', ['b']);
+test(selectAtMacro, -1, 'abc', ['c']);
+test(selectAtMacro, -2, 'abc', ['b']);
+test(selectAtMacro, 3, 'abc', [undefined]);
+test(selectAtMacro, -4, 'abc', [undefined]);
+
+test(selectAtMacro, 0, undefined, [undefined]);
+test(selectAtMacro, 0, null, [undefined]);
+test(selectAtMacro, 0, 0, [undefined]);
+test(selectAtMacro, 0, true, [undefined]);
+
+const updateAtMacro = (t, index, value, input, expected) => {
   const result = update([$at(index), $set(value)], input);
   t.deepEqual(result, expected);
 };
 
-atMacro.title = (title, index, value) => `$at(${index}) $set(${util.inspect(value)})`;
+updateAtMacro.title = (title, index, value) => `$at(${index}) $set(${util.inspect(value)})`;
 
-test(atMacro, 0, 'X', ['a', 'b', 'c'], ['X', 'b', 'c']);
-test(atMacro, 1, 'X', ['a', 'b', 'c'], ['a', 'X', 'c']);
-test(atMacro, -1, 'X', ['a', 'b', 'c'], ['a', 'b', 'X']);
-test(atMacro, -2, 'X', ['a', 'b', 'c'], ['a', 'X', 'c']);
-test(atMacro, 3, 'X', ['a', 'b', 'c'], ['a', 'b', 'c', 'X']);
-test(atMacro, 4, 'X', ['a', 'b', 'c'], ['a', 'b', 'c', undefined, 'X']);
-test(atMacro, -4, 'X', ['a', 'b', 'c'], ['X', 'a', 'b', 'c']);
-test(atMacro, -5, 'X', ['a', 'b', 'c'], ['X', undefined, 'a', 'b', 'c']);
-test(atMacro, 0, $none, ['a', 'b', 'c'], ['b', 'c']);
-test(atMacro, 1, $none, ['a', 'b', 'c'], ['a', 'c']);
-test(atMacro, -1, $none, ['a', 'b', 'c'], ['a', 'b']);
-test(atMacro, -2, $none, ['a', 'b', 'c'], ['a', 'c']);
-test(atMacro, 3, $none, ['a', 'b', 'c'], ['a', 'b', 'c']);
-test(atMacro, -4, $none, ['a', 'b', 'c'], ['a', 'b', 'c']);
+test(updateAtMacro, 0, 'X', ['a', 'b', 'c'], ['X', 'b', 'c']);
+test(updateAtMacro, 1, 'X', ['a', 'b', 'c'], ['a', 'X', 'c']);
+test(updateAtMacro, -1, 'X', ['a', 'b', 'c'], ['a', 'b', 'X']);
+test(updateAtMacro, -2, 'X', ['a', 'b', 'c'], ['a', 'X', 'c']);
+test(updateAtMacro, 3, 'X', ['a', 'b', 'c'], ['a', 'b', 'c', 'X']);
+test(updateAtMacro, 4, 'X', ['a', 'b', 'c'], ['a', 'b', 'c', undefined, 'X']);
+test(updateAtMacro, -4, 'X', ['a', 'b', 'c'], ['X', 'a', 'b', 'c']);
+test(updateAtMacro, -5, 'X', ['a', 'b', 'c'], ['X', undefined, 'a', 'b', 'c']);
+test(updateAtMacro, 0, $none, ['a', 'b', 'c'], ['b', 'c']);
+test(updateAtMacro, 1, $none, ['a', 'b', 'c'], ['a', 'c']);
+test(updateAtMacro, -1, $none, ['a', 'b', 'c'], ['a', 'b']);
+test(updateAtMacro, -2, $none, ['a', 'b', 'c'], ['a', 'c']);
+test(updateAtMacro, 3, $none, ['a', 'b', 'c'], ['a', 'b', 'c']);
+test(updateAtMacro, -4, $none, ['a', 'b', 'c'], ['a', 'b', 'c']);
 
-test(atMacro, 0, 'X', {a: 1, b: 2, c: 3}, {a: 'X', b: 2, c: 3});
-test(atMacro, 1, 'X', {a: 1, b: 2, c: 3}, {a: 1, b: 'X', c: 3});
-test(atMacro, -1, 'X', {a: 1, b: 2, c: 3}, {a: 1, b: 2, c: 'X'});
-test(atMacro, -2, 'X', {a: 1, b: 2, c: 3}, {a: 1, b: 'X', c: 3});
-test(atMacro, 3, $none, {a: 1, b: 2, c: 3}, {a: 1, b: 2, c: 3});
-test(atMacro, -4, $none, {a: 1, b: 2, c: 3}, {a: 1, b: 2, c: 3});
-test(atMacro, 0, $none, {a: 1, b: 2, c: 3}, {b: 2, c: 3});
-test(atMacro, 1, $none, {a: 1, b: 2, c: 3}, {a: 1, c: 3});
-test(atMacro, -1, $none, {a: 1, b: 2, c: 3}, {a: 1, b: 2});
-test(atMacro, -2, $none, {a: 1, b: 2, c: 3}, {a: 1, c: 3});
-test(atMacro, 3, $none, {a: 1, b: 2, c: 3}, {a: 1, b: 2, c: 3});
-test(atMacro, -4, $none, {a: 1, b: 2, c: 3}, {a: 1, b: 2, c: 3});
+test(updateAtMacro, 0, 'X', {a: 1, b: 2, c: 3}, {a: 'X', b: 2, c: 3});
+test(updateAtMacro, 1, 'X', {a: 1, b: 2, c: 3}, {a: 1, b: 'X', c: 3});
+test(updateAtMacro, -1, 'X', {a: 1, b: 2, c: 3}, {a: 1, b: 2, c: 'X'});
+test(updateAtMacro, -2, 'X', {a: 1, b: 2, c: 3}, {a: 1, b: 'X', c: 3});
+test(updateAtMacro, 3, $none, {a: 1, b: 2, c: 3}, {a: 1, b: 2, c: 3});
+test(updateAtMacro, -4, $none, {a: 1, b: 2, c: 3}, {a: 1, b: 2, c: 3});
+test(updateAtMacro, 0, $none, {a: 1, b: 2, c: 3}, {b: 2, c: 3});
+test(updateAtMacro, 1, $none, {a: 1, b: 2, c: 3}, {a: 1, c: 3});
+test(updateAtMacro, -1, $none, {a: 1, b: 2, c: 3}, {a: 1, b: 2});
+test(updateAtMacro, -2, $none, {a: 1, b: 2, c: 3}, {a: 1, c: 3});
+test(updateAtMacro, 3, $none, {a: 1, b: 2, c: 3}, {a: 1, b: 2, c: 3});
+test(updateAtMacro, -4, $none, {a: 1, b: 2, c: 3}, {a: 1, b: 2, c: 3});
 
-test(atMacro, 0, 'X', 'abc', 'Xbc');
-test(atMacro, 1, 'X', 'abc', 'aXc');
-test(atMacro, -1, 'X', 'abc', 'abX');
-test(atMacro, -2, 'X', 'abc', 'aXc');
-test(atMacro, 3, 'X', 'abc', 'abcX');
-test(atMacro, 4, 'X', 'abc', 'abc X');
-test(atMacro, -4, 'X', 'abc', 'Xabc');
-test(atMacro, -5, 'X', 'abc', 'X abc');
-test(atMacro, 0, $none, 'abc', 'bc');
-test(atMacro, 1, $none, 'abc', 'ac');
-test(atMacro, -1, $none, 'abc', 'ab');
-test(atMacro, -2, $none, 'abc', 'ac');
-test(atMacro, 3, $none, 'abc', 'abc');
-test(atMacro, -4, $none, 'abc', 'abc');
+test(updateAtMacro, 0, 'X', 'abc', 'Xbc');
+test(updateAtMacro, 1, 'X', 'abc', 'aXc');
+test(updateAtMacro, -1, 'X', 'abc', 'abX');
+test(updateAtMacro, -2, 'X', 'abc', 'aXc');
+test(updateAtMacro, 3, 'X', 'abc', 'abcX');
+test(updateAtMacro, 4, 'X', 'abc', 'abc X');
+test(updateAtMacro, -4, 'X', 'abc', 'Xabc');
+test(updateAtMacro, -5, 'X', 'abc', 'X abc');
+test(updateAtMacro, 0, $none, 'abc', 'bc');
+test(updateAtMacro, 1, $none, 'abc', 'ac');
+test(updateAtMacro, -1, $none, 'abc', 'ab');
+test(updateAtMacro, -2, $none, 'abc', 'ac');
+test(updateAtMacro, 3, $none, 'abc', 'abc');
+test(updateAtMacro, -4, $none, 'abc', 'abc');
 
-test(atMacro, 0, 'X', undefined, undefined);
-test(atMacro, 0, 'X', null, null);
-test(atMacro, 0, 'X', 0, 0);
-test(atMacro, 0, 'X', true, true);
+test(updateAtMacro, 0, 'X', undefined, undefined);
+test(updateAtMacro, 0, 'X', null, null);
+test(updateAtMacro, 0, 'X', 0, 0);
+test(updateAtMacro, 0, 'X', true, true);
