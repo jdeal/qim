0ff77aa0a1e774b2368b5b4258a899469743f217
Justin Deal <justin.deal@gmail.com>
2017-11-06 22:45:50 -0600

    fix copy-pasta mistake

--- test/$end.js
+++ test/$end.js
@@ -28,7 +28,7 @@ test(selectEndMacro, 'abc', ['']);
 test(selectEndMacro, 5, new Error());
 test(selectEndMacro, undefined, new Error());
 
-const updateBeginMacro = (t, beginArray, input, expected) => {
+const updateEndMacro = (t, beginArray, input, expected) => {
   if (expected instanceof Error) {
     t.throws(() => update([$end, $set(beginArray)], input));
     return;
@@ -37,31 +37,31 @@ const updateBeginMacro = (t, beginArray, input, expected) => {
   t.deepEqual(result, expected);
 };
 
-updateBeginMacro.title = (title, beginArray, input) => `update $end of ${util.inspect(input)} with ${util.inspect(beginArray)}`;
+updateEndMacro.title = (title, beginArray, input) => `update $end of ${util.inspect(input)} with ${util.inspect(beginArray)}`;
 
-test(updateBeginMacro, 'X', ['a', 'b', 'c'], ['a', 'b', 'c', 'X']);
-test(updateBeginMacro, ['X'], ['a', 'b', 'c'], ['a', 'b', 'c', 'X']);
-test(updateBeginMacro, [], ['a', 'b', 'c'], ['a', 'b', 'c']);
-test(updateBeginMacro, $none, ['a', 'b', 'c'], ['a', 'b', 'c']);
-test(updateBeginMacro, {x: 1}, ['a', 'b', 'c'], ['a', 'b', 'c', {x: 1}]);
-test(updateBeginMacro, undefined, ['a', 'b', 'c'], ['a', 'b', 'c', undefined]);
+test(updateEndMacro, 'X', ['a', 'b', 'c'], ['a', 'b', 'c', 'X']);
+test(updateEndMacro, ['X'], ['a', 'b', 'c'], ['a', 'b', 'c', 'X']);
+test(updateEndMacro, [], ['a', 'b', 'c'], ['a', 'b', 'c']);
+test(updateEndMacro, $none, ['a', 'b', 'c'], ['a', 'b', 'c']);
+test(updateEndMacro, {x: 1}, ['a', 'b', 'c'], ['a', 'b', 'c', {x: 1}]);
+test(updateEndMacro, undefined, ['a', 'b', 'c'], ['a', 'b', 'c', undefined]);
 
-test(updateBeginMacro, {x: 10}, {a: 1, b: 2, c: 3}, {a: 1, b: 2, c: 3, x: 10});
-test(updateBeginMacro, {}, {a: 1, b: 2, c: 3}, {a: 1, b: 2, c: 3});
-test(updateBeginMacro, $none, {a: 1, b: 2, c: 3}, {a: 1, b: 2, c: 3});
-test(updateBeginMacro, [10], {a: 1, b: 2, c: 3}, {0: 10, a: 1, b: 2, c: 3});
-test(updateBeginMacro, 'x', {a: 1, b: 2, c: 3}, {0: 'x', a: 1, b: 2, c: 3});
-test(updateBeginMacro, undefined, {a: 1, b: 2, c: 3}, new Error());
-test(updateBeginMacro, 1, {a: 1, b: 2, c: 3}, new Error());
+test(updateEndMacro, {x: 10}, {a: 1, b: 2, c: 3}, {a: 1, b: 2, c: 3, x: 10});
+test(updateEndMacro, {}, {a: 1, b: 2, c: 3}, {a: 1, b: 2, c: 3});
+test(updateEndMacro, $none, {a: 1, b: 2, c: 3}, {a: 1, b: 2, c: 3});
+test(updateEndMacro, [10], {a: 1, b: 2, c: 3}, {0: 10, a: 1, b: 2, c: 3});
+test(updateEndMacro, 'x', {a: 1, b: 2, c: 3}, {0: 'x', a: 1, b: 2, c: 3});
+test(updateEndMacro, undefined, {a: 1, b: 2, c: 3}, new Error());
+test(updateEndMacro, 1, {a: 1, b: 2, c: 3}, new Error());
 
-test(updateBeginMacro, 'X', 'abc', 'abcX');
-test(updateBeginMacro, ['X'], 'abc', 'abcX');
-test(updateBeginMacro, '', 'abc', 'abc');
-test(updateBeginMacro, [], 'abc', 'abc');
-test(updateBeginMacro, $none, 'abc', 'abc');
-test(updateBeginMacro, 5, 'abc', 'abc5');
-test(updateBeginMacro, undefined, 'abc', 'abcundefined');
+test(updateEndMacro, 'X', 'abc', 'abcX');
+test(updateEndMacro, ['X'], 'abc', 'abcX');
+test(updateEndMacro, '', 'abc', 'abc');
+test(updateEndMacro, [], 'abc', 'abc');
+test(updateEndMacro, $none, 'abc', 'abc');
+test(updateEndMacro, 5, 'abc', 'abc5');
+test(updateEndMacro, undefined, 'abc', 'abcundefined');
 
-test(updateBeginMacro, ['X'], 0, new Error());
-test(updateBeginMacro, ['X'], true, new Error());
-test(updateBeginMacro, ['X'], undefined, new Error());
+test(updateEndMacro, ['X'], 0, new Error());
+test(updateEndMacro, ['X'], true, new Error());
+test(updateEndMacro, ['X'], undefined, new Error());
