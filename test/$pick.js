120a4c503df6ccbc3f130e5f3eb69c73576ea993
Justin Deal <justin.deal@gmail.com>
2017-11-08 08:43:25 -0600

    add more $pick tests

--- test/$pick.js
+++ test/$pick.js
@@ -8,12 +8,61 @@ import {
   $each,
   $pick,
   $set,
-  $apply
+  $apply,
+  $none
 } from 'qim/src';
 
 const increment = value => value + 1;
+const toUpperCase = s => s.toUpperCase();
 
-test('select each from pick', t => {
+test('select pick from object', t => {
+  t.deepEqual(
+    select(
+      [$pick('a', 'c')],
+      {a: 1, b: 2, c: 3}
+    ),
+    [{a: 1, c: 3}]
+  );
+});
+
+test('update pick of object', t => {
+  t.deepEqual(
+    update(
+      [$pick('a', 'c'), $set({a: 2})],
+      {a: 1, b: 2, c: 3}
+    ),
+    {a: 2, b: 2}
+  );
+});
+
+test('update pick of object to $none', t => {
+  t.deepEqual(
+    update(
+      [$pick('a', 'c'), $none],
+      {a: 1, b: 2, c: 3}
+    ),
+    {b: 2}
+  );
+});
+
+test('select pick from object with flattening', t => {
+  t.deepEqual(
+    select(
+      [$pick('a', ['c', 'e'])],
+      {a: 1, b: 2, c: 3, d: 4, e: 5}
+    ),
+    [{a: 1, c: 3, e: 5}]
+  );
+});
+
+test('select pick from array', t => {
+  t.deepEqual(
+    select([$pick(0, 2)], ['a', 'b', 'c']),
+    [['a', 'c']]
+  );
+});
+
+test('select each from pick from object', t => {
   t.deepEqual(
     select(
       [$pick('joe', 'mary'), $each, 'name'],
@@ -23,10 +72,17 @@ test('select each from pick', t => {
   );
 });
 
-test('update each of pick', t => {
+test('update each of pick of object', t => {
   t.deepEqual(
     update([$pick('x', 'y'), $each, $apply(increment)], {x: 1, y: 1, z: 1}),
     {x: 2, y: 2, z: 1}
+  );
+});
+
+test('update each of pick of array', t => {
+  t.deepEqual(
+    update([$pick(0, 2), $each, $apply(toUpperCase)], ['a', 'b', 'c']),
+    ['A', 'b', 'C']
   );
 });
 
