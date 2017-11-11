1f20e305b9010a50c5935242946e5ffc243d93ef
Justin Deal <justin.deal@gmail.com>
2017-11-10 18:40:36 -0600

    remove macro from $default and reformat

--- test/$default.js
+++ test/$default.js
@@ -1,41 +1,48 @@
 import test from 'ava';
-import util from 'util';
 
 import 'babel-core/register';
 
-import {
-  select,
-  update,
-  $default,
-  $at,
-  $set
-} from 'qim/src';
+import { select, update, $default, $at, $set } from 'qim/src';
 
-const selectDefaultMacro = (t, path, input, expected) => {
-  const result = select(path, input);
-  t.deepEqual(result, expected);
-};
+test('select property from default object', t =>
+  t.deepEqual(select(['a', $default({}), 'b'], {}), []));
 
-selectDefaultMacro.title = (title, path, input) => `select $path of ${util.inspect(input)}`;
+test('select default value from property of default object', t =>
+  t.deepEqual(select(['a', $default({}), 'b', $default(0)], {}), [0]));
 
-test(selectDefaultMacro, ['a', $default({}), 'b'], {}, []);
-test(selectDefaultMacro, ['a', $default({}), 'b', $default(0)], {}, [0]);
-test(selectDefaultMacro, [$at(0), $default('x')], '', ['x']);
+test('select default string from index of string', t =>
+  t.deepEqual(select([$at(0), $default('x')], ''), ['x']));
 
-const updateDefaultMacro = (t, path, input, expected) => {
-  const result = update(path, input);
-  t.deepEqual(result, expected);
-};
+test('update property of default object to number', t =>
+  t.deepEqual(update(['a', $default({}), 'b', $set(1)], {}), { a: { b: 1 } }));
 
-updateDefaultMacro.title = (title, path, input) => `update $path of ${util.inspect(input)}`;
+test('update index of default array to string', t =>
+  t.deepEqual(update(['a', $default([]), 0, $set('x')], {}), { a: ['x'] }));
 
-test(updateDefaultMacro, ['a', $default({}), 'b', $set(1)], {}, {a: {b: 1}});
-test(updateDefaultMacro, ['a', $default([]), 0, $set('x')], {}, {a: ['x']});
-test(updateDefaultMacro, ['a', $default({b: 0}), 'c', $set(1)], {}, {a: {b: 0, c: 1}});
-test(updateDefaultMacro, ['a', $default(['x']), 1, $set('y')], {}, {a: ['x', 'y']});
-test(updateDefaultMacro, [$default({})], undefined, {});
-test(updateDefaultMacro, [$default({}), 'a', $set(1)], undefined, {a: 1});
-test(updateDefaultMacro, [$default([])], undefined, []);
-test(updateDefaultMacro, [$default([]), 0, $set('x')], undefined, ['x']);
-test(updateDefaultMacro, [0, $default('x')], [], ['x']);
-test(updateDefaultMacro, [$at(0), $default('x')], '', 'x');
+test('update with default non-empty object', t =>
+  t.deepEqual(update(['a', $default({ b: 0 }), 'c', $set(1)], {}), {
+    a: { b: 0, c: 1 }
+  }));
+
+test('update with default non-empty array', t =>
+  t.deepEqual(update(['a', $default(['x']), 1, $set('y')], {}), {
+    a: ['x', 'y']
+  }));
+
+test('update default of undefined', t =>
+  t.deepEqual(update([$default({})], undefined), {}));
+
+test('update default of undefined with set property', t =>
+  t.deepEqual(update([$default({}), 'a', $set(1)], undefined), { a: 1 }));
+
+test('update default of undefined to array', t =>
+  t.deepEqual(update([$default([])], undefined), []));
+
+test('update default of undefined to array and set index', t =>
+  t.deepEqual(update([$default([]), 0, $set('x')], undefined), ['x']));
+
+test('update default of array and set index to string', t =>
+  t.deepEqual(update([0, $default('x')], []), ['x']));
+
+test('update default of array and set position to string', t =>
+  t.deepEqual(update([$at(0), $default('x')], ''), 'x'));
